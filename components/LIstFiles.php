fcc8ac3a495ca82420f1efbfcd65ccaf558231e8
ofthewildfire <103148299+ofthewildfire@users.noreply.github.com>
2025-03-12 21:25:58 +0200

    Refactor Plugin and ListFiles components for improved clarity and functionality

--- components/LIstFiles.php
+++ components/LIstFiles.php
@@ -1,51 +1,45 @@
 <?php
 namespace NiaInteractive\ListFiles\Components;
 
-class ListFiles extends \Cms\Classes\ComponentBase
+use Cms\Classes\ComponentBase;
+use Media\Classes\MediaLibrary; // Import MediaLibrary
+
+class ListFiles extends ComponentBase
 {
-
-    public $fileList = 'lol';
-
+    public $fileList = [];
 
     public function componentDetails()
     {
         return [
             'name' => 'List Files',
-            'description' => 'Displays list of files for given directory.'
+            'description' => 'Displays list of files for a given directory.'
         ];
     }
-
 
     public function defineProperties()
     {
         return [
             'directoryPath' => [
-                 'title'             => 'Folder Path',
-                 'description'       => 'This folder\'s files will be listed',
-                 'default'           => '/',
-                 'required'          => true,
-                 'type'              => 'string',
-                 'validationMessage' => 'Please add path its required.'
+                'title'             => 'Folder Path',
+                'description'       => 'This folder\'s files will be listed',
+                'default'           => '/',
+                'required'          => true,
+                'type'              => 'string',
+                'validationMessage' => 'Please add a valid path, it is required.'
             ]
         ];
     }
 
-
     public function onRender()
     {
-
         $folder = $this->property('directoryPath');
-        $mediaLib = \Media\Classes\MediaLibrar::instance();
+        $mediaLib = MediaLibrary::instance(); // Corrected class name
 
-        // it will return us MediaLibraryItem instance
-        $this->fileList = $mediaLib->listFolderContents($folder);
-
-        // dd($this->fileList);
-        // This code will be executed before the default component
-        // markup is rendered on the page or layout.
-
-        // $this->page['var'] = 'Maximum items allowed: ' . ;
+        // Ensure the directory exists before fetching files
+        if ($mediaLib->folderExists($folder)) {
+            $this->fileList = $mediaLib->listFolderContents($folder);
+        } else {
+            $this->fileList = [];
+        }
     }
-
-
 }
